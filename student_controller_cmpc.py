3eb22bdc0a861177d868bace9a96a4b206f6cae7
Rico Xu <ricox020304@gmail.com>
2023-10-02 12:07:26 -0400

    Added Files

---
+++ student_controller_cmpc.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cvxpy as cp
+
+def Setup_Derivative(param):
+    ## this function is optional
+
+    ## TODO
+
+    return # TODO
+
+def Student_Controller_LQR(x_bar, u_bar, x0, Fun_Jac_dt, param):
+    # TODO
+    
+    return # TODO 
+
+def Student_Controller_CMPC(x_bar, u_bar, x0, Fun_Jac_dt, param):
+    # TODO
+
+    return # TODO

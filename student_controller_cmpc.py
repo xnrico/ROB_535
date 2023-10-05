57d58108e961ab33514ac48c2866850453dade34
Rico Xu <ricox020304@gmail.com>
2023-10-05 16:09:34 -0400

    !

--- student_controller_cmpc.py
+++ student_controller_cmpc.py
@@ -47,9 +47,9 @@ def Student_Controller_LQR(x_bar, u_bar, x0, Fun_Jac_dt, param):
     dim_state = x_bar.shape[1]
     N = len(x_bar) - 1  # Preview horizon
 
-    p = 10000
-    q = 300
-    r = 750
+    p = 100
+    q = 1
+    r = 20
 
     P = np.eye(dim_state) * p
     Q = np.eye(dim_state) * q
